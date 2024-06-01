6602542b2c9f213213703f16d373d9641955f219
keglvir <virag.kegl@gmail.com>
2024-06-01 19:17:43 +0200

    matches

--- pages/2_your_matches.py
+++ pages/2_your_matches.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sklearn import metrics
 
 from constants import CON_KEY, TABLE_NAME
 
@@ -7,7 +8,17 @@ try:
     con = st.session_state[CON_KEY]
     #PRIO1 make this pretty
     df = pd.read_sql(TABLE_NAME, con)
-    st.dataframe(df.assign(s=lambda df: df.loc[:, ["player_1", "player_2"]].sum(axis=1)).sort_values("s", ascending=False))
+    df = df.assign(s=lambda df: df.loc[:, ["player_1", "player_2"]].sum(axis=1)).sort_values("s", ascending=False)
+    match_df = (df[df["s"] == 2])
+
+    st.dataframe(match_df)
+
+    p1 = df["player_1"]
+    p2 = df["player_2"]
+
+    confusion_matrix = metrics.confusion_matrix(p1, p2)
+    st.write("Confusion Matrix:")
+    st.write(confusion_matrix)
 
 except:
     st.write("Dataframe not found. Please try again on the main page")
